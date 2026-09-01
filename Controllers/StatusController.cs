8e2cf09c65de9b17efabf102ef69223a8df79701
USSER1 <USSER1@OUS-BABY>
2026-09-01 10:35:01 +0200

    Add project files.

---
+++ Controllers/StatusController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITSM.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            return Ok("ITSM API is ready and online");
+        }
+    }
+}
---
+++ Controllers/SystemInfoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITSM.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SystemInfoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("IT Service Management Platform \nVersion 1.0 \nStatus: Development");
+        }
+    }
+}
